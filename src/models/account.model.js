f0abfd865ea802b9feb815289f6477319ff87820
Akshat <akshat0746@gmail.com>
2026-08-03 23:21:10 +0530

    fixed some bugs

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,4 +1,4 @@
-const mongoose=rerquire("mongoose")
+const mongoose=require("mongoose")
 
 const accountSchema=new mongoose.Schema({
     user:{
@@ -8,9 +8,11 @@ const accountSchema=new mongoose.Schema({
         index:true
     },
     status:{
+        type:String,
         enum:{
             values:["ACTIVE","FROZEN","CLOSED"],
-            message:"Status can be either ACTIVE,FROZEN or CLOSED"
+            message:"Status can be either ACTIVE,FROZEN or CLOSED",
+            default:"ACTIVE"
         }
     },
     currency:{
@@ -19,7 +21,7 @@ const accountSchema=new mongoose.Schema({
         default:"INR"
     }
 },{
-    timestaamps:true
+    timestamps:true
 })
 
 accountSchema.index({user:1,status:1})
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const authMiddleware=require("../middleware/auth.middleware")
+const accountController=require("../controllers/account.controller")
 
 
+const router=express.Router()
 
-const router=express.router()
-
-router.post("/",authMiddleware.authMiddleware)
+router.post("/",authMiddleware.authMiddleware,accountController.createAccountController)
 
 
 module.exports=router
